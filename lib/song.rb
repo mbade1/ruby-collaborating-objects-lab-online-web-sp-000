57612f2a636d023653f1bce9f859735b3ff75110
Michael Bade <mbade314@gmail.com>
2020-05-20 19:35:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,10 +21,10 @@ class Song
     artist.add_song(self)
   end
 
-  def self.new_by_filename(filename)
-    song_info = filename.chomp(".mp3").split(" - ")
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
     song = Song.new(song_info[1])
     song.artist_name = song_info[0]
-    Artist.add_song(song)
+    song
   end
 end
